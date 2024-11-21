38199f4d08c8c7c55e817284c6a5c5b5bb82499a
RayYPF <69739902+RayYPF@users.noreply.github.com>
2024-11-21 00:24:42 -0600

    11/21 Form html and css completed. Working on js for transferring data to buy page.

--- Final Project/sell.js
+++ Final Project/sell.js
@@ -1,17 +1,77 @@
-function autoPreviewImage(event) {
-    var file = event.target.files[0];
-    
-    if (file && file.type.startsWith('image/')) {
-        var reader = new FileReader();
-        
-        reader.onload = function() {
-            var output = document.getElementById('preview');
-            output.src = reader.result;
-            output.style.display = 'block';
-        }
-        
-        reader.readAsDataURL(file);
-    } else {
-        alert("Please select a valid image file.");
-    }
+const form = document.getElementById("sellForm");
+
+function validate() {
+    fail = validateFullName();
+    fail += validateEmail();
+
+    if(fail==""){showInfo();}
+    else{alert(fail); return false;}
 }
+
+function validateFullName(){
+    var field1 = document.forms["sellForm"]["firstname"].value;
+    var field2 = document.forms["sellForm"]["lastname"].value;
+    if(field1 == "" || field2 == "") {return "No Name was entered.\n"}
+    else if(/[0-9]/.test(field1) || /[0-9]/.test(field2)) {return "Your name should not contain any number.\n"}
+    else {return "";}
+}
+
+function validateEmail() {
+    var field = document.forms["sellForm"]["email"].value;
+    if(field == "") {return "No Email was entered.\n"}
+    else if (!((field.indexOf(".") > 0) && (field.indexOf("@") > 0)) || /[^a-zA-Z0-9.@_-]/.test(field)) {return "The Email address is invalid.\n";}
+    else {return "";}
+}
+
+function previewImages(files) {
+
+    for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+    
+        if (!file.type.startsWith("image/")) {
+          continue;
+        }
+    
+        const img = document.createElement("img");
+        img.classList.add("obj");
+        img.file = file;
+        img.style.maxWidth = "150px";
+        img.style.maxHeight = "150px";
+        document.getElementById("imagePreviewContainer").appendChild(img);
+    
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          img.src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+
+        img.onclick = function () {
+            expandImg(img.src);
+        }
+      }
+}
+
+function expandImg(src) {
+   const imageContainer = document.getElementById("expandImg");
+    const expandImg = document.getElementById("expandImage");
+
+    imageContainer.style.display = "block";
+    expandImg.src = src;
+
+    const closeButton = imageContainer.querySelector(".close");
+    closeButton.onclick = function () {
+        imageContainer.style.display = "none";
+    };
+}
+
+
+// if (form) {
+//     form.addEventListener("submit", (event) => {
+//         event.preventDefault();
+
+//         const formData = new Formdata(form);
+//         const searchString
+//     })
+// }
+
+
